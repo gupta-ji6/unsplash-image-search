2546dc43028673d8855cbf55fd07035326636368
gupta-ji6 <ayushgupta197@gmail.com>
2021-05-29 20:55:27 +0530

    :sparkles: Add header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header>
+      <div className='header_title'>Unsplash Image Search</div>
+    </header>
+  );
+}
+
+export default Header;
